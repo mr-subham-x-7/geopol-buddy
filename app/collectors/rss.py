c7a10ab1417aac782fd63d8086ab166347a06ea8
mr-subham-x-7 <subhamjr07@gmail.com>
2026-07-17 11:41:08 +0530

    Update rss.py

--- app/collectors/rss.py
+++ app/collectors/rss.py
@@ -1,1 +1,13 @@
-empty 
+from .base import BaseCollector
+
+
+class RSSCollector(BaseCollector):
+    """Collector for RSS news feeds."""
+
+    def fetch(self):
+        """Fetch RSS data."""
+        pass
+
+    def parse(self, raw_data):
+        """Parse RSS data."""
+        pass
