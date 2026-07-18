3f2451a1cb94cba9dea6e8832eaa19aead331918
mr-subham-x-7 <subhamjr07@gmail.com>
2026-07-18 12:16:10 +0530

    Refactor send_message function to accept dynamic text

--- app/telegram_client.py
+++ app/telegram_client.py
@@ -2,10 +2,20 @@ from telegram import Bot
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 
-async def send_test_message():
+async def send_message(message):
+    """Send any message to Telegram."""
+
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
     await bot.send_message(
         chat_id=TELEGRAM_CHAT_ID,
-        text="🚀 Hello! Geopol Buddy is now connected to Telegram."
+        text=message,
+    )
+
+
+async def send_test_message():
+    """Send a test message."""
+
+    await send_message(
+        "🚀 Hello! Geopol Buddy is now connected to Telegram."
     )
