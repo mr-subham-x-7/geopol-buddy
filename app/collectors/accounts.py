d41f37bb78448e33670b81375f1333e27cbb3f81
mr-subham-x-7 <subhamjr07@gmail.com>
2026-07-18 21:00:04 +0530

    Update accounts.py

--- app/collectors/accounts.py
+++ app/collectors/accounts.py
@@ -15,6 +15,7 @@ RSS_FEEDS = [
         "name": "BBC US & Canada",
         "url": "https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml",
     },
+    {
            "name": "BBC China",
         "url": "https://feeds.bbci.co.uk/news/world/asia/china/rss.xml",
     },
